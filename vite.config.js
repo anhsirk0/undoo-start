e944bd51ccabc49ac4a6ca7aaa1f2aa0ce2004a4
krishna <krishna404@yandex.com>
2024-10-28 15:36:12 +0530

    update screenshots

--- vite.config.js
+++ vite.config.js
@@ -6,17 +6,17 @@ import { VitePWA } from "vite-plugin-pwa";
 export default defineConfig({
   plugins: [
     react({ include: ["**/*.res.mjs"] }),
-    VitePWA({
-      manifest: {
-        name: "Undoo Startpage",
-        short_name: "Undoo",
-        start_url: "/",
-        display: "standalone",
-        background_color: "#ffffff",
-        theme_color: "#ffffff",
-        scope: "/",
-        icons: [{ src: "/undoo.svg", type: "image/svg+xml", sizes: "any" }],
-      },
-    }),
+    // VitePWA({
+    //   manifest: {
+    //     name: "Undoo Startpage",
+    //     short_name: "Undoo",
+    //     start_url: "/",
+    //     display: "standalone",
+    //     background_color: "#ffffff",
+    //     theme_color: "#ffffff",
+    //     scope: "/",
+    //     icons: [{ src: "/undoo.svg", type: "image/svg+xml", sizes: "any" }],
+    //   },
+    // }),
   ],
 });
